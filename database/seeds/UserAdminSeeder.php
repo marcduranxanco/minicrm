c29b5e5270c70da0b364ef6ce03716843d6e0860
MarcDuran <marcduranxanco@hotmail.com>
2021-01-16 21:32:37 +0100

    ADD: User admin seed

---
+++ database/seeds/UserAdminSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Administrator',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
