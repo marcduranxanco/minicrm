176f3f919fb6cb6ac34e576f757e7172ae7c9dde
MarcDuran <marcduranxanco@hotmail.com>
2021-01-17 21:33:14 +0100

    UPDATE: employees views

---
+++ resources/views/employees/edit.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.minicrm')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Edit employee</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('employees.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('employees.update',$employee->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>First Name:</strong>
+                    <input type="text" name="first_name" value="{{ $employee->first_name }}" class="form-control" placeholder="First name">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Last Name:</strong>
+                    <input type="text" name="last_name" value="{{ $employee->last_name }}" class="form-control" placeholder="Last name">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Company:</strong>
+                    <input type="text" name="company_id" value="{{ $employee->company_id }}" class="form-control" placeholder="Company">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Email:</strong>
+                    <input type="text" name="email" value="{{ $employee->email }}" class="form-control" placeholder="Email">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Phone:</strong>
+                    <input type="text" name="phone" value="{{ $employee->phone }}" class="form-control" placeholder="Phone">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+
+    </form>
+@endsection
---
+++ resources/views/employees/show.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.minicrm')
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show employee</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('employees.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>First name:</strong>
+                {{ $employee->first_name }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Last name:</strong>
+                {{ $employee->last_name }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Email:</strong>
+                {{ $employee->email }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Phone:</strong>
+                {{ $employee->phone }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Company:</strong>
+                {{ $employee->company->name }}
+            </div>
+        </div>
+    </div>
+@endsection
