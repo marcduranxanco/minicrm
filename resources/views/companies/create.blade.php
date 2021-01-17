d8a72f51893d9b32c79a0ceeddcc0b510de8b7ea
MarcDuran <marcduranxanco@hotmail.com>
2021-01-17 22:23:03 +0100

    FIX: company create error

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
-            <h2>Add new employee</h2>
+            <h2>Add new company</h2>
         </div>
         <div class="pull-right">
             <a class="btn btn-primary" href="{{ route('companies.index') }}"> Back</a>
