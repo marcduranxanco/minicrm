2925d682b77a9056bceb49b26e3d16ddaaa64f6c
MarcDuran <marcduranxanco@hotmail.com>
2021-01-17 21:33:49 +0100

    UPDATE: Routes adding companies and employees

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes([
     'register' => false // Dissable Registration Route
   ]);
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('companies','CompanyController');
+Route::resource('employees','EmployeeController');
