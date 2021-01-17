34bd8786e792a969114ffdc7033abe49755ee707
MarcDuran <marcduranxanco@hotmail.com>
2021-01-17 22:24:11 +0100

    UPDATE: layout

--- resources/views/layouts/minicrm.blade.php
+++ resources/views/layouts/minicrm.blade.php
@@ -2,17 +2,12 @@
 <html>
 <head>
     <title>Minicrm</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
 </head>
 <body>
 
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Mini CRM</h2>
-        </div>
-    </div>
-</div>
+@include('layouts.nav')
 
 <div class="container">
     @yield('content')
