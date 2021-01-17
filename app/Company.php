6982367a80edc49b63ef884ef745e564c4e567b0
MarcDuran <marcduranxanco@hotmail.com>
2021-01-17 19:57:12 +0100

    UPDATE: Company and employees models

--- app/Company.php
+++ app/Company.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
+    protected $guarded = array();
+
     public function Employee(){
         return $this->hasMany('App\Employee');
     }
--- app/Employee.php
+++ app/Employee.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
+    protected $guarded = array();
+
     public function Company(){
         return $this->belongsTo('App\Company', 'company_id');
     }
